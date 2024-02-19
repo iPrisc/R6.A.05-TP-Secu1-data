e5d70c4d3c8037f5171dfcbb1ffd7c6c8b38aaa4
Florent <florent.lelion@gmail.com>
2024-02-19 01:28:24 +0100

    etape 3

--- src/plugins/jwt.js
+++ src/plugins/jwt.js
@@ -1,8 +1,25 @@
-import fp from 'fastify-plugin'
-import {readFileSync} from 'fs'
+import fp from 'fastify-plugin';
+import fastifyJwt from '@fastify/jwt';
+import { readFileSync } from 'fs';
+import path from 'path';
 
 export default fp(async function (app, opts) {
+    const privateKeyPath = path.join(__dirname, '..', '..', '..', 'R6.A.05-TP-Secu1-auth', '.ssl', 'ecdsa.key');
+    const publicKeyPath = path.join(__dirname, '..', '..', '..', 'R6.A.05-TP-Secu1-auth', '.ssl', 'ecdsa.pub');
+    const privateKey = readFileSync(privateKeyPath);
+    const publicKey = readFileSync(publicKeyPath);
 
-    // A compléter
-
-})
+    app.register(fastifyJwt, {
+        secret: {
+            private: privateKey,
+            public: publicKey
+        },
+        sign: {
+            algorithm: 'ES256',
+            issuer: 'info.iutparis.fr'
+        },
+        verify: {
+            algorithms: ['ES256']
+        }
+    });
+});
